9230ce43b178b92fa613a2cc2d444fb25900cc4c
satria97 <satria.code97@gmail.com>
2023-01-09 10:22:34 +0700

    postgres database implemented

---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable camelcase */
+const mapDBToModel = ({ id, title, tags, body, created_at, updated_at }) => ({
+	id,
+	title,
+	tags,
+	body,
+	createdAt: created_at,
+	updatedAt: updated_at,
+});
+
+module.exports = { mapDBToModel };
